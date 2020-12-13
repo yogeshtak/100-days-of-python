a7869be863e7e9bb5480d0d5c42aca06afb35dcc
Yogesh Tak <yogesh.tak@outlook.com>
2020-12-13 15:59:53 +0530

    Project 1
    Band Name Generator - Project one

---
+++ day1_bandname_generator.py
@@ -0,0 +1,12 @@
+#day1
+#13-Dec-2020
+
+#welcome title
+print("Welcome to Band Name Generator")
+
+#input city and pet
+city = str(input("Enter the city you grew up in? \n"))
+pet = str(input("Enter the name of your pet? \n"))
+
+#print city and pet together
+print(f'Your Band name should be {city + " "+ pet}')
