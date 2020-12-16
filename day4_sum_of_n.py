02427f0fbb3a53f5333f0ac2195675bb6f4ba053
Yogesh Tak <yogesh.tak@outlook.com>
2020-12-16 23:11:42 +0530

    day 4 project 4
    Sum of first n numbers

---
+++ day4_sum_of_n.py
@@ -0,0 +1,18 @@
+#function to calculate sum of n numbers
+def sumofn():
+    #validating input for positive number
+  while True:
+    n = input("Enter a positive integer?")
+    try:
+      val = int(n)
+      if val < 0:
+        print("This is not positive integer")
+        continue
+      break
+    except ValueError:
+      print("This is not an int")
+    #formula for sum of n numbers
+  sum1 = (val*(val+1))/2
+  print(f"Sum of first {val} positive numbers is: {sum1}")
+
+sumofn()
