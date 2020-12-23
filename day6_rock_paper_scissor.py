990f490d78766089f3c7d52156242c178adf59cd
Yogesh Tak <yogesh.tak@outlook.com>
2020-12-23 22:06:00 +0530

    Completed rock paper scissor

--- day6_rock_paper_scissor.py
+++ day6_rock_paper_scissor.py
@@ -18,7 +18,7 @@ paper = '''
 ---.__________)
 '''
 
-scissors = '''
+scissor = '''
     _______
 ---'   ____)____
           ______)
@@ -29,7 +29,31 @@ scissors = '''
 
 user_ask = int(input("Select your option? 1 for Rock, 2 for Paper and 3 for Scissor"))
 
-computer_opt = random.randint(0,2)
+computer_opt = random.randint(1,3)
 
 if computer_opt == user_ask:
-    
+    print("Game Tied")
+
+if computer_opt == 1 and user_ask == 2:
+    print("You won, computer selected Rock")
+    print(rock)
+
+if computer_opt == 1 and user_ask == 3:
+    print("You lose, computer selected Rock")
+    print(rock)
+
+if computer_opt == 2 and user_ask == 1:
+    print("You lose, Computer selected Paper")
+    print(paper)
+
+if computer_opt == 2 and user_ask == 3:
+    print("You won, computer selected Paper")
+    print(paper)
+
+if computer_opt == 3 and user_ask == 1:
+    print("You won, computer selected Scissor")
+    print(scissor)  
+
+if computer_opt == 3 and user_ask == 2:
+    print("You lose, computer selected Scissor")
+    print(scissor)
