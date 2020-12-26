b4b30dc41b3a21e323e4f310fa35f494cd122281
Yogesh Tak <yogesh.tak@outlook.com>
2020-12-26 22:52:58 +0530

    day9_hangman_game

---
+++ day9_hangman.py
@@ -0,0 +1,8 @@
+word_list = ['abruptly', 'absurd','bikini','cycle','duplex', 'dwarves', 
+'embezzle', 'equip','faking', 'fishhook', 'funny', 'gabby', 'galaxy',
+'hyphen','icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jockey', 'jogging',
+'joking','khaki', 'kilobyte','lengths', 'lucky', 'luxury','oxygen', 'pajama',]
+
+from hangman_art import stages, logo
+
+print(logo)
